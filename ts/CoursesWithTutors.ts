955c7104ef80e33173432d6f79bbeea29f6abe90
Jordan Cutler <jdc219@lehigh.edu>
2017-11-03 13:38:47 -0400

    Remove comment

--- ts/CoursesWithTutors.ts
+++ ts/CoursesWithTutors.ts
@@ -4,20 +4,6 @@ class CoursesWithTutors {
     private static readonly NAME = "CoursesWithTutors";
     private static readonly COURSESWITHTUTORSROUTE = "/api/courses/haveTutors";
     public static init() {
-        // $.ajax({
-        //         url: CoursesWithTutors.COURSESWITHTUTORSROUTE + "?userId=" + User.userId() + "&sessionToken=" + User.sessionToken(),
-        //         type: "GET",
-        //         contentType: "application/json",
-        //         dataType: "json",
-        //         success: function(data: any) {
-        //             let courses = CoursesWithTutors.getCoursesArrayFromJson(data);
-        //             CoursesWithTutors.setHandlebarsTemplate(courses);
-        //         },
-        //         error: function(data: any) {
-        //             window.alert("Failed to retrieve courses with tutors. Please refresh the page and try again.");
-        //         }
-        //     }
-        // );
         HttpRequestUtil.GetRequest(CoursesWithTutors.COURSESWITHTUTORSROUTE, HttpRequestUtil.getSessionInfoJson(),
             function(data: any) {
                 let courses = CoursesWithTutors.getCoursesArrayFromJson(data);
