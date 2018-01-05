b5c65f5f0d288a16d17d770aca370cb0330c4304
Jordan Cutler <jdc219@lehigh.edu>
2018-01-05 18:31:51 -0500

    set default grade to B+

--- nextdoortutorweb/src/app/tutor-application/tutor-application.component.ts
+++ nextdoortutorweb/src/app/tutor-application/tutor-application.component.ts
@@ -1,14 +1,11 @@
-import {
-  AfterContentChecked, AfterContentInit, AfterViewInit, Component, NgZone, OnChanges, OnDestroy,
-  OnInit
-} from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Course } from '../shared/course/course.model';
 import { CourseService } from '../shared/course/course.service';
 import { Grade } from '../shared/tutor/tutor-model/grade.model';
 import { Semester } from '../shared/tutor/tutor-model/semester.model';
-import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { PreloaderService } from '../shared/preloader/preloader.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tutor-application',
@@ -16,9 +13,9 @@ import { PreloaderService } from '../shared/preloader/preloader.service';
   styleUrls: ['./tutor-application.component.css']
 })
 export class TutorApplicationComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('applicationForm') applicationForm: NgForm;
   coursesUserIsNotTutoring: Course[];
 
-  coursesUserIsNotTutoringObservable: Observable<Course[]>;
   private coursesUserIsNotTutoringSubscription: Subscription;
   validGrades: Grade[];
   validSemesters: string[];
@@ -26,45 +23,47 @@ export class TutorApplicationComponent implements OnInit, AfterViewInit, OnDestr
 
   hourlyRate: string;
   defaultCheckedValue = true;
-  constructor(private courseService: CourseService, private preloaderService: PreloaderService) { }
+  defaultGrade: string;
+
+  constructor(private courseService: CourseService, private preloaderService: PreloaderService) {
+  }
 
   ngOnInit() {
-    console.log('ng on init');
-    this.coursesUserIsNotTutoringObservable = this.courseService.getCoursesCurrentUserHasntTutoredBefore();
-    // this.coursesUserIsNotTutoringSubscription = this.coursesUserIsNotTutoringObservable.subscribe(
-    //   (courses: Course[]) => {
-    //     this.preloaderService.show();
-    //     setTimeout(() => {
-    //       this.coursesUserIsNotTutoring = courses;
-    //       console.log('finished assigning courses');
-    //     }, 1000);
-    //
-    //     $('select').material_select();
-    //
-    //     setTimeout(() => {
-    //       $('select').material_select();
-    //       this.preloaderService.hide();
-    //       console.log('called material select');
-    //     }, 1500);
-    //   }
-    // );
+    this.coursesUserIsNotTutoringSubscription = this.courseService.getCoursesCurrentUserHasntTutoredBefore().subscribe(
+      (courses: Course[]) => {
+        this.coursesUserIsNotTutoring = courses;
+        this.setUpIntervalToCheckForCoursesUpdatedOnDomEvent(courses.length - 1);
+      }
+    );
     this.validGrades = Grade.VALID_GRADES;
     this.validSemesters = Semester.VALID_SEMESTERS;
     this.validYears = Semester.VALID_YEARS;
     this.hourlyRate = '20';
+    this.defaultGrade = 'B+';
   }
 
   onSubmit(event: Event) {
+    
+  }
 
+  setUpIntervalToCheckForCoursesUpdatedOnDomEvent(indexOfLastElement: number) {
+    this.preloaderService.show();
+    const intervalReference = setInterval(() => {
+      if ($('#course').find('option[value=' + indexOfLastElement + ']').length) {
+        $('select').material_select();
+        this.preloaderService.hide();
+        clearInterval(intervalReference);
+      }
+    }, 200);
   }
 
   ngAfterViewInit() {
-    $('.material-select').material_select();
+    $('select').material_select();
     $('input.character-count').characterCounter();
   }
 
   ngOnDestroy() {
-    // this.coursesUserIsNotTutoringSubscription.unsubscribe();
+    this.coursesUserIsNotTutoringSubscription.unsubscribe();
   }
 
 }
