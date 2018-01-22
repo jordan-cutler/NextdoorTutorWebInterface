20ad00f39b976ada04c3717e6827fb03b9336595
Jordan Cutler <jdc219@lehigh.edu>
2018-01-21 20:51:28 -0500

    Add error handling for sendgrid email limit

--- nextdoortutorweb/src/app/find-tutor/tutor-list/email-tutor-modal/email-tutor-modal.component.ts
+++ nextdoortutorweb/src/app/find-tutor/tutor-list/email-tutor-modal/email-tutor-modal.component.ts
@@ -6,6 +6,7 @@ import { PreloaderService } from '../../../core/preloader/preloader.service';
 import { DataNeededToFormEmailToTutor } from './DataNeededToFormEmailToTutor';
 import { FormValidity } from '../../../shared/FormValidity';
 import { MaterializeAction } from 'angular2-materialize';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-email-tutor-modal',
@@ -110,6 +111,23 @@ export class EmailTutorModalComponent implements OnInit, AfterViewInit {
           Materialize.toast('Failed to send email. Please try again soon.', 3000);
         }
         this.preloaderService.hide();
+      },
+      (error: HttpErrorResponse) => {
+        this.preloaderService.hide();
+        const userEmailLimitDescription =
+          'You have reached the maximum number of email requests you can make per day. You can send more tomorrow.';
+        const applicationEmailLimitDescription =
+          'NextdoorTutor has reached their limit for sending emails for the day. Users will be able to send more tomorrow.';
+        if (error.error.description === userEmailLimitDescription) {
+          Materialize.toast(userEmailLimitDescription + ' In the meantime, you may email them directly at ' + tutorEmail, 7000);
+        } else if (error.error.description === applicationEmailLimitDescription) {
+          Materialize.toast(
+            applicationEmailLimitDescription +
+            ' We apologize for any inconvenience. You may send an email directly to the tutor at ' + tutorEmail, 8000
+          );
+        } else {
+          Materialize.toast('Failed to send email. Please try again soon.', 3000);
+        }
       }
     );
   }
